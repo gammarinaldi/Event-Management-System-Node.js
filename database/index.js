85e95396875345329d7615f3019ae51606db108b
gammarinaldi <gammarinaldi@yahoo.com>
2019-01-22 17:41:56 +0700

    First

--- database/index.js
+++ database/index.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql');
 
-// const conn = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'ammagg291088',
-//     database: 'gammapedia',
-//     port: 3306
-// });
-
 const conn = mysql.createConnection({
-    host: 'db4free.net',
-    user: 'badgrav1',
+    host: 'localhost',
+    user: 'root',
     password: 'ammagg291088',
-    database: 'gammapedia1',
+    database: 'gammapedia',
     port: 3306
 });
+
+// const conn = mysql.createConnection({
+//     host: 'db4free.net',
+//     user: 'badgrav1',
+//     password: 'ammagg291088',
+//     database: 'gammapedia1',
+//     port: 3306
+// });
 
 module.exports = conn;
